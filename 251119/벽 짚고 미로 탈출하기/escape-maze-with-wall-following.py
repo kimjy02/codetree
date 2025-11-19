eced3eacf176502cd573d712995d950d3d3c19c8
김주연 <kimjuyeon02@gmail.com>
2025-11-19 23:52:22 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 1091ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

--- 251119/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 251119/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -23,31 +23,34 @@ while True:
     wall_i, wall_j = move_dct[(move_idx+1) % 4]
     wi, wj = ni + wall_i, nj + wall_j
 
-    if grid[ni][nj] == "#":
-        # 반시계 회전
-        move_idx = (4 + (move_idx -1)) % 4
-
-    else:
+    if 0 > ni or N <= ni or 0 > nj or N <= nj:
         time += 1
-        if 0 > ni or N <= ni or 0 > nj or N <= nj:
-            break
-        elif 0 <= wi < N and 0 <= wj < N:
-            if grid[wi][wj] == "#": # 전진 + 이동 방향 유지
-                if visited[ni][nj] == 1:
-                    time = -1
-                    break
-                visited[ni][nj] = 1
-                i, j = ni, nj
+        break
 
-            else: # 전진 + 이동 방향 시계 90 회전 + 전진
-                time += 1 # 전진 했다치고 여기서 시간 늘려주기
-                move_idx = (move_idx+1)%4
-                change_i, change_j = move_dct[move_idx]
-                ni, nj = ni + change_i, nj + change_j
-                if visited[ni][nj] == 1:
-                    time = -1
-                    break
-                visited[ni][nj] = 1
-                i, j = ni, nj
+    if 0 <= ni < N and 0 <= nj < N:
+        if grid[ni][nj] == "#":
+            # 반시계 회전
+            move_idx = (4 + (move_idx -1)) % 4
+
+        else:
+            time += 1
+            if 0 <= wi < N and 0 <= wj < N:
+                if grid[wi][wj] == "#": # 전진 + 이동 방향 유지
+                    if visited[ni][nj] == 1:
+                        time = -1
+                        break
+                    visited[ni][nj] = 1
+                    i, j = ni, nj
+
+                else: # 전진 + 이동 방향 시계 90 회전 + 전진
+                    time += 1 # 전진 했다치고 여기서 시간 늘려주기
+                    move_idx = (move_idx+1)%4
+                    change_i, change_j = move_dct[move_idx]
+                    ni, nj = ni + change_i, nj + change_j
+                    if visited[ni][nj] == 1:
+                        time = -1
+                        break
+                    visited[ni][nj] = 1
+                    i, j = ni, nj
 
 print(time)
